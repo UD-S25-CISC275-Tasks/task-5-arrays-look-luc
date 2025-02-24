6399be6ca5831cf393f29c9a4cd2bff5fcc70389
Luc De Nardi <denardiluc@gmail.com>
2025-02-23 19:25:57 -0500

    forgot was not allowed to have for loops

--- src/arrays.ts
+++ src/arrays.ts
@@ -19,10 +19,7 @@ export function bookEndList(numbers: number[]): number[] {
  * number has been tripled (multiplied by 3).
  */
 export function tripleNumbers(numbers: number[]): number[] {
-    const tripled = [];
-    for (let i = 0; i < numbers.length; i++) {
-        tripled.push(numbers[i] * 3);
-    }
+    let tripled = numbers.map((num: number): number => num * 3);
     return tripled;
 }
 
@@ -31,15 +28,9 @@ export function tripleNumbers(numbers: number[]): number[] {
  * the number cannot be parsed as an integer, convert it to 0 instead.
  */
 export function stringsToIntegers(numbers: string[]): number[] {
-    const numRet = [];
-    for (let i = 0; i < numbers.length; i++) {
-        if (isNaN(Number(numbers[i]))) {
-            /*Numbers() found from Google's Gemini and isNaN is from vsCode*/
-            numRet.push(0);
-        } else {
-            numRet.push(Number(numbers[i]));
-        }
-    }
+    let numRet = numbers.map((num: string): number =>
+        isNaN(Number(num)) ? 0 : Number(num),
+    );
     return numRet;
 }
 
@@ -51,28 +42,14 @@ export function stringsToIntegers(numbers: string[]): number[] {
  */
 // Remember, you can write functions as lambdas too! They work exactly the same.
 export const removeDollars = (amounts: string[]): number[] => {
-    const amount = [];
-    for (let i = 0; i < amounts.length; i++) {
-        if (amounts[i].length === 1 && amounts[i][0] === "$") {
-            amount.push(0);
-        } else {
-            if (amounts[i][0] === "$") {
-                if (isNaN(Number(amounts[i].substring(1)))) {
-                    /*substring from Google's Gemini*/
-                    amount.push(0);
-                } else {
-                    amount.push(Number(amounts[i].substring(1)));
-                }
-            } else {
-                if (isNaN(Number(amounts[i]))) {
-                    /*substring from Google's Gemini*/
-                    amount.push(0);
-                } else {
-                    amount.push(Number(amounts[i]));
-                }
-            }
-        }
-    }
+    let amount = amounts.map((num: string): number =>
+        num.length === 1 && num[0] === "$" ? 0
+        : num[0] === "$" ?
+            isNaN(Number(num.substring(1))) ? 0
+            :   Number(num.substring(1))
+        : isNaN(Number(num)) ? 0
+        : Number(num),
+    );
 
     return amount;
 };
@@ -83,17 +60,15 @@ export const removeDollars = (amounts: string[]): number[] => {
  * in question marks ("?").
  */
 export const shoutIfExclaiming = (messages: string[]): string[] => {
-    const message: string[] = [];
-    for (const str of messages) {
-        if (str[str.length - 1] !== "?") {
+    return messages
+        .filter((str) => str[str.length - 1] !== "?")
+        .map((str: string): string => {
             if (str[str.length - 1] === "!") {
-                message.push(str.toUpperCase());
+                return str.toUpperCase();
             } else {
-                message.push(str);
+                return str;
             }
-        }
-    }
-    return message;
+        });
 };
 
 /**
@@ -101,12 +76,7 @@ export const shoutIfExclaiming = (messages: string[]): string[] => {
  * 4 letters long.
  */
 export function countShortWords(words: string[]): number {
-    let num4 = 0;
-    for (const word of words) {
-        if (word.length < 4) {
-            num4 += 1;
-        }
-    }
+    let num4: number = words.filter((word) => word.length < 4).length;
     return num4;
 }
 
@@ -116,18 +86,13 @@ export function countShortWords(words: string[]): number {
  * then return true.
  */
 export function allRGB(colors: string[]): boolean {
-    let RGB: string[] = ["red", "blue", "green"];
     let counter: number = 0;
     if (colors.length === 0) {
         return true;
     } else {
-        for (const color of colors) {
-            for (const i of RGB) {
-                if (i === color) {
-                    counter += 1;
-                }
-            }
-        }
+        counter += colors.filter((color) => color === "red").length;
+        counter += colors.filter((color) => color === "green").length;
+        counter += colors.filter((color) => color === "blue").length;
     }
     if (counter === colors.length) {
         return true;
@@ -149,14 +114,8 @@ export function makeMath(addends: number[]): string {
     if (addends.length === 0) {
         return "0=0";
     } else {
-        for (let i = 0; i < addends.length; i++) {
-            if (i !== addends.length - 1) {
-                itemStr += addends[i] + "+";
-            } else {
-                itemStr += addends[i];
-            }
-            numSum += addends[i];
-        }
+        numSum = addends.reduce((sum: number, num: number) => sum + num, 0);
+        itemStr = addends.join("+");
     }
     return numSum.toString() + "=" + itemStr;
 }
@@ -173,18 +132,15 @@ export function makeMath(addends: number[]): string {
 export function injectPositive(values: number[]): number[] {
     let numSum: number = 0;
     let newVal: number[] = [];
-    let indexFirNeg: number = -1;
+    let indexFirNeg: number;
 
     //for loop inspired from Google's Gemini
-    /*this goes through all of the values and see if it is negative and if so, will store that index and stops the for loop
-    before the loop breaks, the sum is updated.*/
-    for (let i = 0; i < values.length; i++) {
-        if (values[i] < 0 && indexFirNeg === -1) {
-            indexFirNeg = i;
-            break;
-        }
-        numSum += values[i];
-    }
+    /*this checks where is the first occurence of a negative number
+    then goes through the original array to add up the sum and place it in the right spot in relation to the negative*/
+    indexFirNeg = values.findIndex((val: number) => val < 0);
+    numSum = values
+        .slice(0, indexFirNeg === -1 ? values.length : indexFirNeg)
+        .reduce((sum: number, num: number) => sum + num, 0);
 
     //overall code inspired from Google's Gemini
     /*This checks if there was a negative ever existed and if so we cut the original array up to the negative and add it to 
